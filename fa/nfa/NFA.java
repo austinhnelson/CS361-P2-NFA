c13347b52b30ace59cab7e4079d031cf6b4e14b7
Austin Nelson <nelsonhaustin@gmail.com>
2022-11-19 21:08:55 -0700

    Added P1 Skeleton Code

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -1,32 +1,109 @@
 package fa.nfa;
 
-import fa.dfa.DFA;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
-public class NFA {
 
-	public void addFinalState(String nextToken) {
-		// TODO Auto-generated method stub
-		
-	}
+    mport fa.dfa.DFA;
+        
 
-	public void addStartState(String startStateName) {
-		// TODO Auto-generated method stub
-		
-	}
+    *
 
+    * 
+         @author patricksantana austinnels
+
+    u
+
+    /* Defining machine variables */
+        rivate NFAState startState;
+
+    p
+
+    /**
+        * Creates a new NFA, default const
+
+    p
+
+    	alphabet = new Linke
+        
+        
+    @Override
 	public void addState(String nextToken) {
-		// TODO Auto-generated method stub
+		NFAState stateToAdd = new NFAState(nextToken);
+		states.add(stateToAdd);
 		
 	}
 
+	@Override
+	public void addStartState(String startStateName) {
+		NFAState stateToAdd = new NFAState(startStateName);
+		states.add(stateToAdd);
+		startState = stateToAdd;
+		
+	}
+
+	@Override
+	public void addFinalState(String nextToken) {
+		NFAState stateToAdd = new NFAState(nextToken, true);
+		states.add(stateToAdd);
+		
+	}
+
+	@Override
 	public void addTransition(String valueOf, char c, String valueOf2) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	@Override
+	public Set<? extends State> getStates() {
+		return states;
+	}
+
+	@Override
+	public Set<? extends State> getFinalStates() {
+		Set<NFAState> finalStates = new LinkedHashSet<NFAState>();
+		for(NFAState itr : states){
+			if(itr.isFinal()){
+				finalStates.add(itr);
+			}
+		}
+		return finalStates;
+	}
+
+	@Override
+	public State getStartState() {
+		return startState;
+	}
+
+	@Override
+	public Set<Character> getABC() {
+		return alphabet;
+	}
+
+	@Override
+	public Set<NFAState> getToState(NFAState from, char onSymb) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<NFAState> eClosure(NFAState s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
 	public DFA getDFA() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	/**
+	 * Returns output for NFA
+	 */
+	public String toString(){
+		return "";
 	}
 
 }
--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,5 +1,57 @@
 package fa.nfa;
 
-public class NFAState {
+import java.util.HashMap;
+import fa.State;
+
+/**
+ * Represents a NFA state and 
+ * its transitions to other states
+ * 
+ * @author patricksantana austinnelson
+ */
+public class NFAState extends State {
+
+	private boolean isFinal;
+
+    HashMap<Character, NFAState> transitionMap; 
+
+    /**
+     * Creates a new DFAState - default constructor
+     * @param name
+     */
+    public NFAState(String name){
+        this.name = name;
+        isFinal = false;
+        transitionMap = new HashMap<Character, NFAState>();
+    }
+
+    /**
+     * Overload construction to set final state 
+     * @param name
+     * @param isFinal whether or not it is a final state
+     */
+    public NFAState(String name, boolean isFinal){
+        this.name = name;
+        this.isFinal = isFinal;
+    }
+
+    /**
+     * Add a transition to the state
+     * 
+     * @param k alphabet symbol
+     * @param toState the state referencing
+     */
+    public void addTransition(char k, NFAState toState){
+        transitionMap.put(k, toState);
+    }
+
+    /**
+     * Returns final state
+     * 
+     * @return whether or not state is final
+     */
+    public boolean isFinal(){
+        return isFinal;
+    }
 
 }
